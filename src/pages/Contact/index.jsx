6591d7ed925f5e6941c7579cd78205a1cbb8ac5c
eltac-malik <eltacem@code.edu.az>
2023-04-27 20:18:17 +0400

    upd

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import style from './Contact.module.css'
+
 export const Contact = () => {
   return (
-    <div>
-      <h1>salam</h1>
+    <div className={style.contact}>
+      
+      <div className={style.form}>
+        <div className={style.contac_info}>salam</div>
+
+        <div className={`${style.form_box} flex items-center justify-center`}>
+            salam
+        </div>
+      </div>
+
     </div>
   )
 }
