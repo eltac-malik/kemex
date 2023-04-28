bf4f7a4cd3d64f589a81da3365a34a3c7a145cd9
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-04-28 22:21:28 +0400

    fix: general

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
+import { BsArrowRight } from 'react-icons/bs';
+
 import style from "./Contact.module.css";
 
 const validationSchema = Yup.object().shape({
@@ -25,7 +27,7 @@ export const Contact = () => {
       <div className={style.form}>
         <div className={`style.contac_info text-start gap-y-14`}>
           <p className={`text-red-600`}>CONTACT US NOW</p>
-          <h2 className="text-5xl	mt-5">
+          <h2 className="text-5xl	mt-5 font-bold">
             HAPPY TO ANSWER ALL <br /> YOUR QUESTIONS
           </h2>
           <p className={`mt-5 `}>Call us for all your questions today.</p>
@@ -76,7 +78,7 @@ export const Contact = () => {
                     name="name"
                     id="name"
                     placeholder=" Name"
-                    className={`appearance-none border-b-2 w-full py-4 px-1 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
+                    className={`border-gray-950 appearance-none border-b-2 w-full py-4 px-1 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
                       errors.name && touched.name ? "border-red-500" : ""
                     }`}
                   />
@@ -93,7 +95,7 @@ export const Contact = () => {
                     name="email"
                     id="email"
                     placeholder="Email"
-                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
+                    className={`border-gray-950 appearance-none border-b-2 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
                       errors.email && touched.email ? "border-red-500" : ""
                     }`}
                   />
@@ -110,7 +112,7 @@ export const Contact = () => {
                     name="phone"
                     id="phone"
                     placeholder="Phone"
-                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
+                    className={`border-gray-950 appearance-none border-b-2 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
                       errors.phone && touched.phone ? "border-red-500" : ""
                     }`}
                   />
@@ -128,7 +130,7 @@ export const Contact = () => {
                     id="textarea"
                     placeholder=" Message"
                     rows={3}
-                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
+                    className={`border-gray-950 appearance-none border-b-2 w-full py-8 px-2 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
                       errors.textarea && touched.textarea
                         ? "border-red-500"
                         : ""
@@ -140,13 +142,17 @@ export const Contact = () => {
                     className="text-red-500"
                   />
                 </div>
-
-                <button
+                    <div
+                    className="text-end"
+                    >
+                    <button
                   type="submit"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded justify-end flex-1"
+                  className="border-2 border-black hover:bg-blue-700 text-black font-bold py-2 px-4 flex"
                 >
-                  Submit
+                  Send message <BsArrowRight className="border-l-2"/>
                 </button>
+                    </div>
+       
               </Form>
             )}
           </Formik>
