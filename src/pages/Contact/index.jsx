6319e6f6d836222127a0fdde68f3d671cb14759e
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-04-28 18:03:41 +0400

    feat: contact-us info add

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -4,6 +4,7 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 import style from "./Contact.module.css";
+
 const validationSchema = Yup.object().shape({
   name: Yup.string().required("Name is required"),
   email: Yup.string().email("Invalid email").required("Email is required"),
@@ -22,7 +23,42 @@ export const Contact = () => {
   return (
     <div className={style.contact}>
       <div className={style.form}>
-        <div className={style.contac_info}>salam</div>
+        <div className={`style.contac_info text-start gap-y-14`}>
+          <p className={`text-red-600`}>CONTACT US NOW</p>
+          <h2 className="text-5xl	mt-5">
+            HAPPY TO ANSWER ALL <br /> YOUR QUESTIONS
+          </h2>
+          <p className={`mt-5 `}>Call us for all your questions today.</p>
+          <div className="flex">
+            <div className={`w-44 mt-5`}>
+              <h3>OUR ADRESSES</h3>
+              <p>Long Hills Dr 351, New York 07078, United States</p>
+            </div>
+            <div className={`w-44 mt-5 ml-10`}>
+              <h3>EMAIL</h3>
+              <p>
+                <a href="mailto:webmaster@example.com">webmaster@example.com</a>
+              </p>
+            </div>
+          </div>
+          <div className="flex">
+            <div className={`w-44 mt-5`}>
+              <h3>PHONES</h3>
+              <p>
+                <a href="tel:+4733378901">+47 333 78 901</a>
+              </p>
+              <p>
+                <a href="tel:+4733378901">+47 333 78 901</a>
+              </p>
+            </div>
+            <div className={`w-44 mt-5 ml-10`}>
+              <h3>SOCIAL</h3>
+              <p>
+                <a href="mailto:webmaster@example.com">webmaster@example.com</a>
+              </p>
+            </div>
+          </div>
+        </div>
 
         <div className={`${style.form_box} flex items-center justify-center`}>
           <Formik
@@ -33,14 +69,14 @@ export const Contact = () => {
             }}
           >
             {({ errors, touched }) => (
-              <Form>
+              <Form className={`w-96`}>
                 <div className="mb-4">
                   <Field
                     type="text"
                     name="name"
                     id="name"
                     placeholder=" Name"
-                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                    className={`appearance-none border-b-2 w-full py-4 px-1 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
                       errors.name && touched.name ? "border-red-500" : ""
                     }`}
                   />
@@ -56,8 +92,8 @@ export const Contact = () => {
                     type="email"
                     name="email"
                     id="email"
-                    placeholder=" Name"
-                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                    placeholder="Email"
+                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
                       errors.email && touched.email ? "border-red-500" : ""
                     }`}
                   />
@@ -74,7 +110,7 @@ export const Contact = () => {
                     name="phone"
                     id="phone"
                     placeholder="Phone"
-                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
                       errors.phone && touched.phone ? "border-red-500" : ""
                     }`}
                   />
@@ -92,7 +128,7 @@ export const Contact = () => {
                     id="textarea"
                     placeholder=" Message"
                     rows={3}
-                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                    className={`appearance-none border-b-2 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
                       errors.textarea && touched.textarea
                         ? "border-red-500"
                         : ""
@@ -107,7 +143,7 @@ export const Contact = () => {
 
                 <button
                   type="submit"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded justify-end flex-1"
                 >
                   Submit
                 </button>
