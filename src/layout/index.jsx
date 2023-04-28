659ea4ec7610c04bdc9d47f9ef49da421af09f40
Eizeek <azez.karimov@gmail.com>
2023-04-28 21:08:33 +0400

    Eizeek/Feature/Layuout/index/Added-visuals/added-formik-form

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,14 +2,15 @@ import * as React from "react";
 import { useSnapshot } from "valtio";
 
 import Drawer from "@mui/material/Drawer";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 import { Navbar } from "#/Navbar";
 import { state } from "state";
 import { widthByPercent } from "shared/common";
 import { useScreen } from "shared/hooks";
-
-import style from './Drawer.module.css'
+import style from "./Drawer.module.css";
 
 export const Layout = ({ children }) => {
   const snap = useSnapshot(state);
@@ -41,18 +42,83 @@ export const Layout = ({ children }) => {
                 width: widthByPercent(isMobile ? 70 : 100),
               }}
             >
-              <span className={`text-stone-800 font-bold p-4`} onClick={closeDrawer}><CloseIcon/></span>
-              <h1
-                className={`text-orange-600 font-2xl`}
-                
+              <span
+                className={`text-stone-800 hover:text-red-400 font-bold cursor-pointer animate-pulse p-4 mt-1`}
+                onClick={closeDrawer}
               >
+                <CloseIcon />
+              </span>
+              <h1 className={`text-orange-600 p-2 text-center text-2xl/[45px]`}>
                 Contact Details
               </h1>
-              <ul>
-                <li>Address: 123 Main Street, City, State</li>
-                <li>Phone: 555-1234</li>
-                <li>Email: example@example.com</li>
+              <ul className="bg-gray-200 p-8 m-2 rounded-lg">
+                <li className="text-lg text-gray-700">
+                  Address: 123 Main Street, City, State
+                </li>
+                <li className="text-lg text-gray-700">Phone: 555-1234</li>
+                <li className="text-lg text-gray-700">
+                  Email: example@example.com
+                </li>
               </ul>
+              <Formik>
+                <Form className="bg-gray-100 rounded-lg p-6 m-2">
+                  <div className="mb-4">
+                    <label><h1 className={`text-orange-600 text-center p-2 text-2xl/[45px]`}>HAPPY TO ANSWER YOUR QUESTIONS</h1></label>
+                    <label
+                      htmlFor="name"
+                      className="block text-gray-700 font-bold mb-2"
+                    >
+                      Name:
+                    </label>
+                    <Field
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                    />
+                    <ErrorMessage name="name" />
+                  </div>
+
+                  <div className="mb-4">
+                    <label
+                      htmlFor="email"
+                      className="block text-gray-700 font-bold mb-2"
+                    >
+                      Email:
+                    </label>
+                    <Field
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                    />
+                    <ErrorMessage name="email" />
+                  </div>
+
+                  <div className="mb-4">
+                    <label
+                      htmlFor="message"
+                      className="block text-gray-700 font-bold mb-2"
+                    >
+                      Message:
+                    </label>
+                    <Field
+                      as="textarea"
+                      id="message"
+                      name="message"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight resize-none"
+                    />
+                    <ErrorMessage name="message\" />{" "}
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="bg-gray-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline animate-pulse"
+                  >
+                    Submit
+                  </button>
+                </Form>
+              </Formik>
             </div>
           </Drawer>
         </React.Fragment>
