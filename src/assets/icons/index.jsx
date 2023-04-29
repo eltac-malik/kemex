f280bd780a5e8fbba6c84e30799287477620d557
eltac-malik <eltacem@code.edu.az>
2023-04-29 11:48:03 +0400

    upd

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -1,7 +1,6 @@
 import { AiOutlinePhone,AiOutlineClose } from "react-icons/ai";
 import { BsArrowRight,BsFacebook,BsInstagram,BsTwitter } from 'react-icons/bs';
 
-export const Phone = (props) => <AiOutlinePhone {...props}/>;
 export const ArrowRight = (props) => <BsArrowRight {...props}/>;
 export const Facebook = (props) => <BsFacebook {...props}/>;
 export const Instagram = (props) => <BsInstagram {...props}/>;
