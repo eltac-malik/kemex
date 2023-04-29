b732191a9edd33a2ff26e0176458801444babe04
ImranHatefi <imranhatefi@gmail.com>
2023-04-29 13:33:05 +0400

    imran|hotfix

--- src/shared/constant/index.js
+++ src/shared/constant/index.js
@@ -4,11 +4,11 @@ export const mainTitles = [
         t2:"Kemex International Qafqaz regionunda ticarət lideridir və neft, mədən, su və qida şirkətlərinin əksəriyyəti ilə işləyir."
     },
     {
-        t1:"Bizimlə işləyin",
+        t1:"Bizimlə işləyin!",
         t2:"Gənc mütəxəssislər komandası tərəfindən yaradılmış rəqabət üstünlüyü etibarlılıq, müxtəliflik və çeviklik vasitəsilə."
     },
     {
-        t1:"Biz nə edirik",
+        t1:"Biz nə edirik?",
         t2:"Kemex International 2018-ci ildə yaradılıb sənaye kimyasının mütəxəssisləri və bu gün tədarükü ilə məşğuldur sənaye kimyası beynəlxalq ticarət mərtəbələrinə."
     }
 ]
