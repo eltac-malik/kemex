7ad3aad71fb6a7d6787b6b43ff149ddfe2bb4e98
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-04-30 12:56:44 +0400

    bug-fix: responsive-design

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -13,8 +13,13 @@ import style from "./Contact.module.css";
 const validationSchema = Yup.object().shape({
   name: Yup.string().required("Name is required"),
   email: Yup.string().email("Invalid email").required("Email is required"),
-  phone: Yup.number().typeError("That doesn't look like a phone number").positive("A phone number can't start with a minus").integer("A phone number can't include a decimal point").min(8).required('Phone number is required'), 
-   message: Yup.string().required("Message is required"),
+  phone: Yup.number()
+    .typeError("That doesn't look like a phone number")
+    .positive("A phone number can't start with a minus")
+    .integer("A phone number can't include a decimal point")
+    .min(8)
+    .required("Phone number is required"),
+  message: Yup.string().required("Message is required"),
 });
 
 const initialValues = {
@@ -28,39 +33,54 @@ export const Contact = () => {
   return (
     <div className={style.contact}>
       <div className={style.form}>
-        <div className={`style.contac_info text-start gap-y-14`}>
+        <div
+          className={`style.contac_info text-start max-[680px]:items-center`}
+        >
           <p className={`text-red-600 text-xl font-bold`}>CONTACT US NOW</p>
-          <h2 className="text-5xl	mt-5 font-bold">
+          <h2 className="text-5xl	mt-5 font-bold npm i react-tilt  max-[680px]:text-2xl ">
             HAPPY TO ANSWER ALL <br /> YOUR QUESTIONS
           </h2>
-          <p className={`mt-5 text-2xl `}>
-            Call us for all your questions today.
+          <p className={`mt-5 text-2xl w-96 max-[680px]:text-xl`}>
+            Kemex International həm Azərbaycanda, həm də Gürcüstanda təmsil
+            olunur və İtaliya, Ukrayna, Belorusiya, Çin, Hindistan və digər
+            olkələrdə yerləşən istehsalçılarla əməkdaşlıq edir.
           </p>
-          <div className="flex">
-            <div className={`w-44 mt-5`}>
-              <h3>OUR ADRESSES</h3>
-              <p>Long Hills Dr 351, New York 07078, United States</p>
-            </div>
-            <div className={`w-44 mt-5 ml-10`}>
-              <h3>EMAIL</h3>
+          <div className="flex flex-wrap">
+            <div className={`w-44 mt-5 max-[680px]:ml-10 max-[680px]:w-full `}>
+              <h3>
+                <b>OUR ADRESSES</b>
+              </h3>
               <p>
-                <a href="mailto:webmaster@example.com">webmaster@example.com</a>
+                Ahmad Rajabli 1/8,
+                <br /> Adore Plaza, 4th floor
+              </p>
+            </div>
+            <div className={`w-44 mt-5 ml-10  max-[680px]:w-full `}>
+              <h3>
+                <b>EMAIL</b>
+              </h3>
+              <p>
+                <a href="mailto: info@chemical.az"> info@chemical.az</a>
               </p>
             </div>
           </div>
-          <div className="flex">
-            <div className={`w-44 mt-5`}>
-              <h3>PHONES</h3>
+          <div className="flex flex-wrap">
+            <div className={`w-44 mt-5 max-[680px]:ml-10`}>
+              <h3>
+                <b>PHONES</b>
+              </h3>
               <p>
-                <a href="tel:+4733378901">+47 333 78 901</a>
+                <a href="tel:+994125255155">+994 12 525 51 55</a>
               </p>
               <p>
-                <a href="tel:+4733378901">+47 333 78 901</a>
+                <a href="tel:+994502913202">+994 50 291 32 02</a>
               </p>
             </div>
             <div className={`w-44 mt-5 ml-10`}>
-              <h3>SOCIAL</h3>
-              <p className="flex gap-5 mt-5  ">
+              <h3>
+                <b>SOCIAL</b>
+              </h3>
+              <p className="flex gap-5 mt-4  ">
                 <Facebook className="hover:text-orange-700" />{" "}
                 <Instagram className="hover:text-orange-700" />{" "}
                 <Twitter className="hover:text-orange-700" />
@@ -69,7 +89,9 @@ export const Contact = () => {
           </div>
         </div>
 
-        <div className={`${style.form_box} flex items-center justify-center`}>
+        <div
+          className={`${style.form_box} flex items-center justify-center max-[680px]:-mt-28`}
+        >
           <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
@@ -78,14 +100,14 @@ export const Contact = () => {
             }}
           >
             {({ errors, touched }) => (
-              <Form className={`w-96`}>
+              <Form className={`w-96 ` }>
                 <div className="mb-4">
                   <Field
                     type="text"
                     name="name"
                     id="name"
                     placeholder=" Name"
-                    className={`border-gray-950 appearance-none border-b-2 w-full py-4 px-1 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
+                    className={`border-gray-950 appearance-none border-b-2 w-full py-4 px-1 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800   ${
                       errors.name && touched.name ? "border-red-500" : ""
                     }`}
                   />
@@ -138,8 +160,7 @@ export const Contact = () => {
                     placeholder=" Message"
                     rows={3}
                     className={`border-gray-950 appearance-none border-b-2 w-full py-8 px-2 text-gray-800 leading-tight focus:outline-none focus:shadow-outline placeholder-gray-800  ${
-                      errors.message && touched.message
-                        ? "border-red-500" : ""
+                      errors.message && touched.message ? "border-red-500" : ""
                     }`}
                   />
                   <ErrorMessage
