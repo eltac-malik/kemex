126b92b98fbf490ab7cc6a061bfa8a97f6139d9c
Zarangul <zarangulbabazade@gmail.com>
2023-05-04 14:36:34 +0400

    bugfix/zarangul/search-bar

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -5,12 +5,11 @@ import { products } from "shared/mock";
 import style from "./Products.module.css";
 import Header from "#/Header/index.jsx";
 import SearchBox from "#/SearchBar";
-import {useTranslation} from 'react-i18next'
-
+import { useTranslation } from "react-i18next";
+import { widthByPercent } from "../../shared/common";
 export const Products = () => {
   const [input, setInput] = useState("");
   const [productList, setProductList] = useState([]);
-
   useEffect(() => setProductList(products), []);
 
   const handleFilter = (e) => {
@@ -22,46 +21,46 @@ export const Products = () => {
   const handleClear = () => {
     setProductList(products);
     setInput("");
-    console.log("last step 1");
   };
 
-  const {t} = useTranslation()
+  const { t } = useTranslation();
   const { i18n } = useTranslation();
 
   useEffect(() => {
-    i18n.changeLanguage(localStorage.getItem('selected_language') || "az");
-  }, ['az','en','ru']);
+    i18n.changeLanguage(localStorage.getItem("selected_language") || "az");
+  }, ["az", "en", "ru"]);
 
-  
   return (
     <div className={`${style.products} h-screen w-full flex bg-gray-800 `}>
       <main className={`${style.first_section} w-full`}>
         <div className="px-10 mt-4 mb-4">
-          <Header title={t('home')} subtitle={t('products')} />
+          <Header title={t("home")} subtitle={t("products")} />
           <div
             className={`${style.search_box} pt-2 pl-2 flex justify-start w-full`}
           >
-            <SearchBox
-              onChange={(e, val) => handleFilter(val)}
-              clear={handleClear}
-            />
+            {(widthByPercent(100) <= 768) && (
+              <SearchBox
+                onChange={(e, val) => handleFilter(val)}
+                clear={handleClear}
+              />
+            )}
           </div>
         </div>
         <div className="flex">
-          <div
-            className={`${style.side_bar} pt-4 pl-4 pr-4 w-auto`}
-            style={{
-              borderRight: "1px solid silver",
-            }}
-          >
-            <SearchBox
-              onChange={(e, val) => handleFilter(val)}
-              clear={handleClear}
-            />
-          </div>
-          <div
-            className="px-10 grid grid-cols-4 gap-4 w-full"
-          >
+          {widthByPercent(100) > 768 && (
+            <div
+              className={`${style.side_bar} pt-4 pl-4 pr-4 w-auto`}
+              style={{
+                borderRight: "1px solid silver",
+              }}
+            >
+              <SearchBox
+                onChange={(e, val) => handleFilter(val)}
+                clear={handleClear}
+              />
+            </div>
+          )}
+          <div className="px-10 grid grid-cols-4 gap-4 w-full">
             {productList
               ?.filter((e) => {
                 if (input === "") {
