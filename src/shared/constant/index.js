111cd2904d2273f3b92af306713a97399956e114
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-05-05 00:00:11 +0400

    test

--- src/shared/constant/index.js
+++ src/shared/constant/index.js
@@ -1,17 +1,16 @@
 
-
 export const mainTitles = [
     {
-        t1:"Biz kimik ?",
-        t2:"Kemex International Qafqaz regionunda ticarət lideridir və neft, mədən, su və qida şirkətlərinin əksəriyyəti ilə işləyir."
+        t1:"t1",
+        t2:"t2a"
     },
     {
-        t1:"Bizimlə işləyin!",
-        t2:"Gənc mütəxəssislər komandası tərəfindən yaradılmış rəqabət üstünlüyü etibarlılıq, müxtəliflik və çeviklik vasitəsilə."
+        t1:"t2b",
+        t2:"t2c"
     },
     {
-        t1:"Biz nə edirik?",
-        t2:"Kemex International 2018-ci ildə yaradılıb sənaye kimyasının mütəxəssisləri və bu gün tədarükü ilə məşğuldur sənaye kimyası beynəlxalq ticarət mərtəbələrinə."
+        t1:"t3a",
+        t2:"t3b"
     }
 ]
 
